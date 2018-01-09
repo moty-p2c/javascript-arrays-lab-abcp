f99d51b1b4eeb07f46cb60cb725e902673293a85
moty pitusy <moty.p2c@gmail.com>
2018-01-09 11:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+let kittens = ["Milo", "Otis", "Garfiels"];
+
+function destructivelyAppendKitten(newCat){
+  return kittens.push(newCat)
+}
+
+function destructivelyPrependKitten(newCat){
+  return kittens.unshift(newCat)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(newCat){
+  let newArr = [...kittens]
+  newArr.push(newCat)
+  return newArr
+}
+
+function prependKitten(newCat){
+  let newArr = [...kittens]
+  newArr.unshift(newCat)
+  return newArr
+}
+
+function removeLastKitten(newCat){
+  let newArr = [...kittens]
+  newArr.pop()
+  return newArr
+}
+
+function removeFirstKitten(newCat){
+  let newArr = [...kittens]
+  newArr.shift()
+  return newArr
+}
+
